a3c973e41ba141ec584ab43cda85e02d354060df
piggy <piggygam1ngg@gmail.com>
2024-01-07 14:16:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const metaViewportThere = document.querySelector("meta[name=viewport]");
 if (!metaViewportThere) {
     const metaViewport = document.createElement("meta");
     metaViewport.setAttribute("name", "viewport");
-    metaViewport.setAttribute("content", "width=device-width, initial-scale=1.0");
+    metaViewport.setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=0");
     document.head.appendChild(metaViewport);
+} else {
+    metaViewportThere.setAttribute("content", "width=device-width, initial-scale=1.0, user-scalable=0");
 }
